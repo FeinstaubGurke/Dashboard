87e06a421b940f57a5e3e93fad9bf792dff0d741
Tino Hager <tino.hager@nager.at>
2024-10-28 17:46:34 +0100

    optimize pdf (wip)

--- src/CreateReport/PdfHelper.cs
+++ src/CreateReport/PdfHelper.cs
@@ -11,7 +11,11 @@ namespace CreateReport
         {
             var builder = new PdfDocumentBuilder
             {
-                ArchiveStandard = PdfAStandard.A1A
+                ArchiveStandard = PdfAStandard.A1A,
+                DocumentInformation = new PdfDocumentBuilder.DocumentInformationBuilder
+                {
+                    Title = "Feinstaub Report"
+                }
             };
 
             var trueTypeFontPath = "Roboto-Regular.ttf";
@@ -27,7 +31,7 @@ namespace CreateReport
                 var chartElementWidth = 9.8;
                 var chartElementHeight = 80;
 
-                var chartElementSpace = 5;
+                var chartElementSpace = 5.0;
                 var chartElementSpaceY = 50;
 
                 var positionY = page.PageSize.Top - ((i + 1) * (chartElementHeight + chartElementSpaceY));
@@ -35,9 +39,21 @@ namespace CreateReport
                 var deviceDataStatistic = deviceDataStatistics[i];
                 var dataPoints = deviceDataStatistic.HourlyStatisticData.OrderBy(o => o.Date).ThenBy(o => o.Hour).ToArray();
 
+                var nextDay = false;
+                var lastDayPositionX = chartElementSpace;
+
                 for (var j = 0; j < dataPoints.Length; j++)
                 {
                     var dataPoint = dataPoints[j];
+
+                    if (j > 0 && dataPoint.Date.DayOfWeek != dataPoints[j - 1].Date.DayOfWeek || j == dataPoints.Length - 1)
+                    {
+                        nextDay = true;
+                    }
+                    else
+                    {
+                        nextDay = false;
+                    }
 
                     var positionX = (j * chartElementWidth) + chartElementSpace;
 
@@ -67,9 +83,19 @@ namespace CreateReport
                     page.SetTextAndFillColor(223, 2, 2); //Dark Red
                     page.DrawRectangle(new PdfPoint(positionX, chartElementVeryPoorPositionY), chartElementWidth, chartElementVeryPoorHeight, 1, fill: true);
 
+                    if (nextDay)
+                    {
+                        page.SetTextAndFillColor(220, 220, 220); //Light Gray
+                        page.DrawRectangle(new PdfPoint(lastDayPositionX, positionY - 8), positionX - lastDayPositionX, 8, lineWidth: 0, fill: true);
+
+                        page.SetTextAndFillColor(10, 10, 10);
+                        page.AddText($"{dataPoint.Date:ddd}", 6, new PdfPoint(lastDayPositionX + 2, positionY - 6), font);
+
+                        lastDayPositionX = positionX;
+                    }
+
                     page.SetTextAndFillColor(100, 100, 100);
-                    page.AddText($"{dataPoint.Date:ddd}", 5, new PdfPoint(positionX, positionY - 6), font);
-                    page.AddText($"{dataPoint.Hour}", 5, new PdfPoint(positionX, positionY - 10), font);
+                    page.AddText($"{dataPoint.Hour}", 5, new PdfPoint(positionX, positionY - 14), font);
                 }
 
                 page.SetTextAndFillColor(0, 0, 0);
@@ -77,13 +103,13 @@ namespace CreateReport
                 var firstDataPoint = dataPoints.FirstOrDefault();
                 if (firstDataPoint != null)
                 {
-                    page.AddText($"{firstDataPoint.Date:yyyy-MM-dd}", 12, new PdfPoint(5, positionY - 20), font);
+                    page.AddText($"{firstDataPoint.Date:yyyy-MM-dd}", 11, new PdfPoint(5, positionY - 25), font);
                 }
 
                 var lastDataPoint = dataPoints.LastOrDefault();
                 if (lastDataPoint != null)
                 {
-                    page.AddText($"{lastDataPoint.Date:yyyy-MM-dd}", 12, new PdfPoint(page.PageSize.Right - 100, positionY - 20), font);
+                    page.AddText($"{lastDataPoint.Date:yyyy-MM-dd}", 11, new PdfPoint(page.PageSize.Right - 100, positionY - 25), font);
                 }
 
                 if (deviceDataStatistic.DeviceTitle != null)
